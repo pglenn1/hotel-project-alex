8870ada0b11db26c53929c9f67c46d14547979d8
Alex Glenn <157517894+pglenn1@users.noreply.github.com>
2024-04-16 17:40:54 -0500

    Update script.js
    java for radio

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-  $('#signupForm').submit(function(event) {
+  $('#registrationForm').submit(function(event) {
     event.preventDefault(); // Prevent default form submission
 
     // Perform form validation here if needed
 
     // Show success message
     $('#successMessage').removeClass('d-none');
+  });
+
+  // Button click event handler
+  $("#btn").on("click", () => {
+    let selectedSize;
+    if ($("input:radio[name='skill']").is(":checked")) {
+      selectedSize = $("input[name='skill']:checked").val();
+    }
+    $('#output').text(selectedSize
+      ? `You selected ${selectedSize}`
+      : `You haven't selected a skill class`);
   });
 });
